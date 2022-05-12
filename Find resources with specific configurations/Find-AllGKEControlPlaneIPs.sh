ac2c4e57f30ce51c355e89c9fd9c8455300553f6
emiliensocchi <emilien.socchi@gmail.com>
2022-05-12 09:58:35 +0200

    Add file

---
+++ Find resources with specific configurations/Find-AllGKEControlPlaneIPs.sh
@@ -0,0 +1,28 @@
+#### Description #################################################################################
+#
+# Indexes all Google Kubernetes Engine (GKE) clusters in an environement and retrieves the IP 
+# address of their control planes.
+#
+####
+
+#! /usr/bin/env/bash
+
+all_gke_control_plane_ips=()
+
+for project in $(gcloud projects list --format="get(projectId)")
+do
+      echo "[*] scraping project: $project"
+
+      ips=$(gcloud container clusters list --project "$project" --format json | jq -r '.[] | .endpoint')
+
+      if [[ "$ips" ]]
+      then
+            all_gke_control_plane_ips+=(${ips[@]})
+      fi
+
+      echo ""
+done
+
+echo "-----"
+echo "GKE clusters in the environement are publicly exposed on the following IP addresses:"
+printf '%s\n' "${all_gke_control_plane_ips[@]}" | sort
